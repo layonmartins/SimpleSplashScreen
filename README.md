b6d6bbfd29337aecd1677cfd726630af4acdc26e
layonfonseca <layon.fonseca@eldorado.org.br>
2022-02-14 10:06:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#An Android app sample of how create an simple Splash Screen
+
+* Kotlin
+* Activity
+* Handler
+
+<img src="splashscreen.gif" width="40%" />
+
+### Reference
+* https://www.youtube.com/watch?v=Q0gRqbtFLcw
---
+++ app/src/main/java/com/layon/splashscreensample/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.layon.splashscreensample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
